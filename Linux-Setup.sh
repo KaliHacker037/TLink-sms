e159137c44df7079a822fd110fd4923e3a2bc8cb
KaliHacker <100252424+KaliLinux037@users.noreply.github.com>
2022-02-24 17:22:40 +0530

    Add files via upload

---
+++ Linux-Setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install curl
+sudo apt install python2
+sudo apt install python3 
+sudo apt install figlet
+sudo apt install wget
+sudo apt install git
+sudo apt install lolcat
